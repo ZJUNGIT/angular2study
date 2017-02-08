a5be976d484c12e37172814e29fdbd05ec1675d0
Jeason <jeason@JeasondeMacBook-Pro.local>
2017-02-08 15:56:11 +0800

    模板驱动型表单 by junjiesun

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component} from "@angular/core";
 import { User } from "./classes/user";
 
-
 @Component({
     selector:"root-app",
     templateUrl:"app/app.component.html",
@@ -9,9 +8,52 @@ import { User } from "./classes/user";
 })
 
 export class AppComponent{
-  user = new User("Jeason","This is password","sunjunjie8023@qq.com","beijing");
+  active = true;
 
+  user = new User("Jeason","This is password","sunjunjie8023@qq.com","beijing");
+  regionList = [
+    {
+      name:'beijing',
+      label:'北京'
+    },
+    {
+      name:"shanghai",
+      label:"上海"
+    },
+    {
+      name:"guangzhou",
+      label:"广州"
+    },
+    {
+      name:"shenzhen",
+      label:"深圳"
+    }
+  ];
   get userInfo() {
       return JSON.stringify(this.user);
   }
+
+  onSubmit(myForm){
+    console.log(myForm);
+  }
+
+  /**
+   * 清空数据，这时候那些验证错误就会被检测到
+   */
+  reset(myForm){
+    this.user = {};
+  }
+
+  reset(myForm){
+    myForm.reset();
+    return false;
+  }
+
+  reset(myForm){
+    this.user = {};
+    this.active = false;
+    setTimeout(() => this.active =true,0);
+    return false;
+  }
+
 }
