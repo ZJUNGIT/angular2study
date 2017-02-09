dfee9182774e81fbb2a247efd42ad3256d0773ca
Jeason <jeason@JeasondeMacBook-Pro.local>
2017-02-09 15:00:18 +0800

    service-demo2 by junjiesun

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -2,8 +2,6 @@ import {Component,OnInit} from "@angular/core";
 import { User } from "./class/User";
 import { UserService } from "./services/user.service";
 import { LogService } from "./services/log.service";
-import { BetterLogger } from "./services/logger-better.service";
-import { LoggerHelp } from "./services/logger-helper.service";
 
 
 @Component({
@@ -11,8 +9,7 @@ import { LoggerHelp } from "./services/logger-helper.service";
     templateUrl:"app/app.component.html",
     styleUrls:["app/app.component.css"],
     providers:    [
-      // LogService,
-      [{provide: LogService, useClass: BetterLogger}],
+      LogService,
       UserService
     ]
 })
--- demo/app/services/logger-helper.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@angular/core";
-
-@Injectable()
-
-export class LoggerHelp{
-  constructor(){
-    console.warn('Just a logger helper!');
-  }
-}
